b4de85e22d3c5960418660b34ef0dda1cd9d92c2
Markus Kopp <markus.kopp@student.uibk.ac.at>
2021-01-05 23:30:21 +0100

    update format and new output file

--- proseminar/08/queens/queens_omp.c
+++ proseminar/08/queens/queens_omp.c
@@ -3,37 +3,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int check_paths(int pos_row, int pos_col, int problem_size, int board[problem_size][problem_size]){
+int check_paths(int pos_row, int pos_col, int problem_size,
+                int board[problem_size][problem_size]) {
 
   int row, col;
-  //check left row
+  // check left row
   for (int col = 0; col < pos_col; ++col) {
-    if (board[pos_row][col]){
-      //printf("left row\n");
+    if (board[pos_row][col]) {
+      // printf("left row\n");
       return 0;
     }
   }
 
-  //check left upper diogonal
-  for (row=pos_row-1, col=pos_col-1; row >= 0 && col >=0; --row, --col) {
-    if (board[row][col]){
-      //printf("left upper diogonal\n");
+  // check left upper diogonal
+  for (row = pos_row - 1, col = pos_col - 1; row >= 0 && col >= 0;
+       --row, --col) {
+    if (board[row][col]) {
+      // printf("left upper diogonal\n");
       return 0;
     }
   }
 
-  //check left lower diogonal
-  for (row=pos_row+1, col=pos_col-1; row < problem_size && col >=0; ++row, --col) {
-    if (board[row][col]){
-      //printf("left lower diogonal\n");
+  // check left lower diogonal
+  for (row = pos_row + 1, col = pos_col - 1; row < problem_size && col >= 0;
+       ++row, --col) {
+    if (board[row][col]) {
+      // printf("left lower diogonal\n");
       return 0;
     }
   }
   return 1;
 }
 
-int req_solve(int problem_size, int board[problem_size][problem_size], int start_col, int *solvenumbers){
-  if (start_col >= problem_size){
+int req_solve(int problem_size, int board[problem_size][problem_size],
+              int start_col, int *solvenumbers) {
+  if (start_col >= problem_size) {
     /* for (int row=0; row < problem_size; ++row) { */
     /*   for (int col=0; col < problem_size; ++col) { */
     /*     printf("%d ", board[row][col]); */
@@ -44,14 +48,14 @@ int req_solve(int problem_size, int board[problem_size][problem_size], int start
     /* printf("---------\n"); */
 #pragma omp atomic
     ++(*solvenumbers);
-    return 0; //stop here normaly with return 1 for he first solution only
+    return 0; // stop here normaly with return 1 for he first solution only
   }
-  
-  for (int row=0; row<problem_size; ++row) {
-    if (check_paths(row, start_col, problem_size, board)){
+
+  for (int row = 0; row < problem_size; ++row) {
+    if (check_paths(row, start_col, problem_size, board)) {
       board[row][start_col] = 1;
 #pragma omp task
-      req_solve(problem_size, board, start_col+1, solvenumbers);
+      req_solve(problem_size, board, start_col + 1, solvenumbers);
 #pragma omp taskwait
       board[row][start_col] = 0;
     }
@@ -59,42 +63,37 @@ int req_solve(int problem_size, int board[problem_size][problem_size], int start
   return 0;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   // 'parsing' optional input parameter = problem size
   int N = 8;
   if (argc > 1) {
     N = atoi(argv[1]);
   }
-  printf("Computing queens problem with N=%d x %d\n", N,N);
+  printf("Computing queens problem with N=%d x %d\n", N, N);
 
   // board is used with [row][columns]
   int board[N][N];
   int solvenumbers = 0;
 
-  for (int row=0; row < N; ++row) {
-    for (int col=0; col < N; ++col) {
+  for (int row = 0; row < N; ++row) {
+    for (int col = 0; col < N; ++col) {
       board[row][col] = 0;
     }
   }
 #pragma omp parallel
   {
 #pragma omp single
-    {
-      req_solve(N, board, 0, &solvenumbers);
-    }
+    { req_solve(N, board, 0, &solvenumbers); }
   }
 
   printf("found solutions=%d\n", solvenumbers);
-  
+
   /* for (int row=0; row < N; ++row) { */
   /*   for (int col=0; col < N; ++col) { */
   /*     printf("%d ", board[row][col]); */
   /*   } */
   /*   printf("\n"); */
   /* } */
-    
-    
+
   return EXIT_SUCCESS;
 }
-
--- proseminar/08/queens/queens_seq.c
+++ proseminar/08/queens/queens_seq.c
@@ -3,37 +3,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int check_paths(int pos_row, int pos_col, int problem_size, int board[problem_size][problem_size]){
+int check_paths(int pos_row, int pos_col, int problem_size,
+                int board[problem_size][problem_size]) {
 
   int row, col;
-  //check left row
+  // check left row
   for (int col = 0; col < pos_col; ++col) {
-    if (board[pos_row][col]){
-      //printf("left row\n");
+    if (board[pos_row][col]) {
+      // printf("left row\n");
       return 0;
     }
   }
 
-  //check left upper diogonal
-  for (row=pos_row-1, col=pos_col-1; row >= 0 && col >=0; --row, --col) {
-    if (board[row][col]){
-      //printf("left upper diogonal\n");
+  // check left upper diogonal
+  for (row = pos_row - 1, col = pos_col - 1; row >= 0 && col >= 0;
+       --row, --col) {
+    if (board[row][col]) {
+      // printf("left upper diogonal\n");
       return 0;
     }
   }
 
-  //check left lower diogonal
-  for (row=pos_row+1, col=pos_col-1; row < problem_size && col >=0; ++row, --col) {
-    if (board[row][col]){
-      //printf("left lower diogonal\n");
+  // check left lower diogonal
+  for (row = pos_row + 1, col = pos_col - 1; row < problem_size && col >= 0;
+       ++row, --col) {
+    if (board[row][col]) {
+      // printf("left lower diogonal\n");
       return 0;
     }
   }
   return 1;
 }
 
-int req_solve(int problem_size, int board[problem_size][problem_size], int start_col, int *solvenumbers){
-  if (start_col >= problem_size){
+int req_solve(int problem_size, int board[problem_size][problem_size],
+              int start_col, int *solvenumbers) {
+  if (start_col >= problem_size) {
     /* for (int row=0; row < problem_size; ++row) { */
     /*   for (int col=0; col < problem_size; ++col) { */
     /*     printf("%d ", board[row][col]); */
@@ -45,12 +49,12 @@ int req_solve(int problem_size, int board[problem_size][problem_size], int start
     ++(*solvenumbers);
     return 0;
   }
-  for (int row=0; row<problem_size; ++row) {
-    if (check_paths(row, start_col, problem_size, board)){
+  for (int row = 0; row < problem_size; ++row) {
+    if (check_paths(row, start_col, problem_size, board)) {
       board[row][start_col] = 1;
 
-      if (req_solve(problem_size, board, start_col+1, solvenumbers)){
-        return 1;        
+      if (req_solve(problem_size, board, start_col + 1, solvenumbers)) {
+        return 1;
       }
       board[row][start_col] = 0;
     }
@@ -58,35 +62,32 @@ int req_solve(int problem_size, int board[problem_size][problem_size], int start
   return 0;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   // 'parsing' optional input parameter = problem size
   int N = 8;
   if (argc > 1) {
     N = atoi(argv[1]);
   }
-  printf("Computing queens problem with N=%d x %d\n", N,N);
+  printf("Computing queens problem with N=%d x %d\n", N, N);
 
   // board is used with [row][columns]
   int board[N][N];
   int solvenumbers = 0;
 
-  for (int row=0; row < N; ++row) {
-    for (int col=0; col < N; ++col) {
+  for (int row = 0; row < N; ++row) {
+    for (int col = 0; col < N; ++col) {
       board[row][col] = 0;
     }
   }
   req_solve(N, board, 0, &solvenumbers);
   printf("found solutions=%d\n", solvenumbers);
-  
+
   /* for (int row=0; row < N; ++row) { */
   /*   for (int col=0; col < N; ++col) { */
   /*     printf("%d ", board[row][col]); */
   /*   } */
   /*   printf("\n"); */
   /* } */
-    
-    
+
   return EXIT_SUCCESS;
 }
-
